2837fa5e54bf021eec171b70e730de99f104bf3b
Bjarne Riis <bjarne@briis.com>
2022-05-22 08:53:45 +0000

    Fixing Issue deprecated `async_get_registry` #67

--- custom_components/weatherbit/__init__.py
+++ custom_components/weatherbit/__init__.py
@@ -168,7 +168,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def _async_get_or_create_nvr_device_in_registry(
     hass: HomeAssistant, entry: ConfigEntry, station_data: BaseDataDescription
 ) -> None:
-    device_registry = await dr.async_get_registry(hass)
+    device_registry = dr.async_get(hass)
 
     device_registry.async_get_or_create(
         config_entry_id=entry.entry_id,
